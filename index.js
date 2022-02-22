b0847abe2e58d7a8203bbaf38bca065bac6331cd
Magdalena Schuetz <m.schuetz@outlook.de>
2022-02-22 12:59:09 +0100

    Add pkg, run npm build to create bin

--- index.js
+++ index.js
@@ -4,14 +4,26 @@ const inquirer = require('inquirer');
 inquirer
   .prompt([
     {
-      name: 'creatingFunctionFile',
+      name: 'functionName',
       message: 'What is the name of your function?',
+    },
+
+    {
+      type: 'checkbox',
+      name: 'fileTypes',
+      message: 'Which kind of file should be created?',
+      choices: ['component', 'spec', 'stories'],
+      validate: (answer) => {
+        if (answer.length < 1) {
+          return 'Please select at least one!';
+        }
+        return true;
+      },
     },
   ])
   .then((answers) => {
-    writeFile(answers.creatingFunctionFile);
-    console.info(
-      `Thank your for your answer. The file ${answers.creatingFunctionFile} has been created!`,
-      answers.creatingFunctionFile
-    );
+    console.log(answers);
+    answers.fileTypes.forEach((type) => {
+      writeFile(answers.functionName, type);
+    });
   });
--- writeFile.js
+++ writeFile.js
@@ -1,9 +1,48 @@
 const fs = require('fs');
-module.exports = function writeFile(name) {
-  fs.writeFileSync(
-    `./${name}.js`,
-    `function ${name}() {
-    
-  }`
-  );
+const template = {
+  component: (name) => {
+    const componentString = `import styled from 'styled-components'
+    export default function ${name}() {
+      return <div>${name}</div>
+    }
+    `;
+    return componentString;
+  },
+
+  spec: (name) => {
+    const specString = `
+    import { render, screen } from '@testing-library/react';
+    //import component to test
+
+    describe('', () => {
+      it('', () => {
+        render(<div />);
+        expect().toBeInTheDocument();
+      });
+    })`;
+    return specString;
+  },
+
+  stories: (name) => {
+    const storiesString = `
+    // import component to render in Storybook;
+
+    export default {
+      title: 'Component/Dahsboard',
+      component: Dashboard,
+    };
+
+    const Template = () => <div />;
+
+    export const NameofComponent = Template.bind({});
+  `;
+    return storiesString;
+  },
+};
+
+module.exports = function writeFile(name, fileType) {
+  const fileName = fileType === 'component' ? `./${name}.js` : `./${name}.${fileType}.js`;
+  const fileString = template[fileType](name);
+
+  fs.writeFileSync(fileName, fileString);
 };
